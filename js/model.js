cfb8f94d9297590077a31fb8e0c4b4fe8d012ab8
Code Monkey King <freesoft.for.people@gmail.com>
2024-09-17 18:15:47 +0300

    fixed additional features side wise

--- js/model.js
+++ js/model.js
@@ -29,7 +29,7 @@ function boardTensor() { /* Convert GUI goban.position to katago model input ten
       bin_inputs[inputBufferChannels * sq_19x19 + 0] = 1.0;
       if (goban.position[sq_21x21] == katago) bin_inputs[inputBufferChannels * sq_19x19 + 1] = 1.0;
       if (goban.position[sq_21x21] == player) bin_inputs[inputBufferChannels * sq_19x19 + 2] = 1.0;
-      if (goban.position[sq_21x21] == goban.BLACK || goban.position[sq_21x21] == goban.WHITE) {
+      if (goban.position[sq_21x21] == katago || goban.position[sq_21x21] == player) {
         let libs_black = 0;
         let libs_white = 0;
         goban.count(sq_21x21, goban.BLACK);
@@ -38,50 +38,49 @@ function boardTensor() { /* Convert GUI goban.position to katago model input ten
         goban.count(sq_21x21, goban.WHITE);
         libs_white = goban.liberties.length;
         goban.restore();
-        //if (libs_black == 1 || libs_white == 1) bin_inputs[inputBufferChannels * sq_19x19 + 3] = 1.0;
-        //if (libs_black == 2 || libs_white == 2) bin_inputs[inputBufferChannels * sq_19x19 + 4] = 1.0;
-        //if (libs_black == 3 || libs_white == 3) bin_inputs[inputBufferChannels * sq_19x19 + 5] = 1.0;
+        if (libs_black == 1 || libs_white == 1) bin_inputs[inputBufferChannels * sq_19x19 + 3] = 1.0;
+        if (libs_black == 2 || libs_white == 2) bin_inputs[inputBufferChannels * sq_19x19 + 4] = 1.0;
+        if (libs_black == 3 || libs_white == 3) bin_inputs[inputBufferChannels * sq_19x19 + 5] = 1.0;
       }
     }
   }
-  /*if (sq_19x19 == goban.ko) bin_inputs[inputBufferChannels * sq_19x19 + 6] = 1.0;
+  if (sq_19x19 == goban.ko) bin_inputs[inputBufferChannels * sq_19x19 + 6] = 1.0;
   let moveIndex = goban.history.length-1;
-  if (moveIndex >= 1 && goban.history[moveIndex-1].side == goban.WHITE) {
+  if (moveIndex >= 1 && goban.history[moveIndex-1].side == player) {
     let prevLoc1 = goban.history[moveIndex-1].move;
     let x = prevLoc1 % 21;
     let y = Math.floor(prevLoc1 / 21);
     if (prevLoc1) bin_inputs[inputBufferChannels * (19 * y + x) + 9] = 1.0;
     else global_inputs[0] = 1.0;
-    if (moveIndex >= 2 && goban.history[moveIndex-2].side == goban.BLACK) {
+    if (moveIndex >= 2 && goban.history[moveIndex-2].side == katago) {
       let prevLoc2 = goban.history[moveIndex-2].move;
       let x = prevLoc2 % 21;
       let y = Math.floor(prevLoc2 / 21);
       if (prevLoc2) bin_inputs[inputBufferChannels * (19 * y + x) + 10] = 1.0;
       else global_inputs[1] = 1.0;
-      if (moveIndex >= 3 && goban.history[moveIndex-3].side == goban.WHITE) {
+      if (moveIndex >= 3 && goban.history[moveIndex-3].side == player) {
         let prevLoc3 = goban.history[moveIndex-3].move;
         let x = prevLoc3 % 21;
         let y = Math.floor(prevLoc3 / 21);
         if (prevLoc3) bin_inputs[inputBufferChannels * (19 * y + x) + 11] = 1.0;
         else global_inputs[2] = 1.0;
-        if (moveIndex >= 4 && goban.history[moveIndex-4].side == goban.BLACK) {
+        if (moveIndex >= 4 && goban.history[moveIndex-4].side == katago) {
           let prevLoc4 = goban.history[moveIndex-4].move;
           let x = prevLoc4 % 21;
           let y = Math.floor(prevLoc4 / 21);
           if (prevLoc4) bin_inputs[inputBufferChannels * (19 * y + x) + 12] = 1.0;
           else global_inputs[3] = 1.0;
-          if (moveIndex >= 5 && goban.history[moveIndex-5].side == goban.WHITE) {
+          if (moveIndex >= 5 && goban.history[moveIndex-5].side == player) {
             let prevLoc5 = goban.history[moveIndex-5].move;
             let x = prevLoc5 % 21;
             let y = Math.floor(prevLoc5 / 21);
-            if (prevLoc5) bin_inputs[inputBufferChannels * (19 * y + x) + 13] = 1.0;
+            if (prevLoc5) { bin_inputs[inputBufferChannels * (19 * y + x) + 13] = 1.0; console.log('feature 13'); }
             else global_inputs[4] = 1.0;
           }
         }
       }
     }
-  }*/
-
+  }
   let selfKomi = (computerSide == goban.WHITE ? komi+1 : -komi);
   global_inputs[5] = selfKomi / 20.0
   return bin_inputs;
@@ -108,6 +107,11 @@ async function play() { /* Query KataGo network */
     let bestMove = 21 * (row_19+1) + (col_19+1);
     if (!goban.play(bestMove, computerSide, false)) {
       alert('Pass');
+      console.log('best move:', col_19, row_19);
+      results[0].print();
+      results[1].print();
+      results[2].print();
+      results[3].print();
       goban.pass();
     }
     goban.refresh();
