6c2aece4ed21ce02e377c1fa6b88622252ceb2e7
Code Monkey King <freesoft.for.people@gmail.com>
2024-09-30 19:00:11 +0300

    trying to fix service worker path

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('my-app-cache').then((cache) => {
       return cache.addAll([
-        '/',
+        '/kata-model-js',
         '/kata-model-js/mobile.html',
         '/kata-model-js/images/icon-192x192.png',
         '/kata-model-js/images/icon-512x512.png'
